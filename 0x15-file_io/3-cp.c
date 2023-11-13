5e309c670fc4c9747c309caac23b0037d2fc78d9
Otmane65 <omar.jabr9019@gmail.com>
2023-11-13 02:52:02 -0800

    test

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,7 +2,10 @@
 #include <stdio.h>
 
 /**
- * test
+ * error_file - checks if files can be opened.
+ * @file_from: file_from.
+ * @file_to: file_to.
+ * @argv: arguments vector.
  * Return: no return.
  */
 void error_file(int file_from, int file_to, char *argv[])
@@ -20,13 +23,15 @@ void error_file(int file_from, int file_to, char *argv[])
 }
 
 /**
- * test
+ * main - check the code for ALX students.
+ * @argc: number of arguments.
+ * @argv: arguments vector.
  * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
 	int file_from, file_to, err_close;
-	ssize_t num_char, nwr;
+	ssize_t nchars, nwr;
 	char buf[1024];
 
 	if (argc != 3)
@@ -39,13 +44,13 @@ int main(int argc, char *argv[])
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
 	error_file(file_from, file_to, argv);
 
-	num_char = 1024;
-	while (num_char == 1024)
+	nchars = 1024;
+	while (nchars == 1024)
 	{
-		num_char = read(file_from, buf, 1024);
-		if (num_char == -1)
+		nchars = read(file_from, buf, 1024);
+		if (nchars == -1)
 			error_file(-1, 0, argv);
-		nwr = write(file_to, buf, num_char);
+		nwr = write(file_to, buf, nchars);
 		if (nwr == -1)
 			error_file(0, -1, argv);
 	}
